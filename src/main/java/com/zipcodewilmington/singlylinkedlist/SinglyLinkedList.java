cf356e437ecee8b9a6fd4550310f1caab036b8b8
r-m-hufford <ryan@rmhufford.com>
2021-03-28 19:35:15 -0400

    getting started

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,65 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    class Node {
+        Node next;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void remove() {}
+
+    public boolean contains() {
+        return false;
+    }
+
+    public int find() {
+        //returns index of first instance or -1
+        return -1;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public void copy() {}
+
+    public void sort() {}
+
+
+    public void display() {
+        Node current = head;
+
+        if (head == null) {
+            System.out.println("Empty List");
+        }
+        while (current != null) {
+            System.out.println( current.data + " ");
+            current = current.next;
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void add() {
+        SinglyLinkedList sList = new SinglyLinkedList();
+
+        sList.add(1);
+
+        sList.display();
+    }
+
+
+
 }
